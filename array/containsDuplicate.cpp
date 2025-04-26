c3b92902cf97c4cfc51d5a065f4ab76722db5ed2
Aditya Raj <adityaraj7320@gmail.com>
2025-04-26 16:26:44 +0530

    1

--- array/containsDuplicate.cpp
+++ array/containsDuplicate.cpp
@@ -8,11 +8,35 @@ Output: true
 Example 2:
 Input: nums = [1, 2, 3, 4]
 Output: false
+solution: using hash map to store and check for duplicate, this case set data structure is used.
 */
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+
+class Solution
+{
+public:
+    bool hasDuplicate(std::vector<int> &nums)
+    {
+        std::unordered_set<int> set;
+        for (int i : nums)
+        {
+            if (set.find(i) != set.end())
+            {
+                return true;
+            }
+            set.insert(i);
+        }
+        return false;
+    }
+};
 
 int main()
 {
-    std::print("this is working\n");
+    Solution sol;
+    std::vector<int> nums1 = {1, 2, 3, 3};
+    std::vector<int> nums2 = {1, 2, 3, 4};
+    std::print("{}\n", sol.hasDuplicate(nums2));
     return 0;
 }
